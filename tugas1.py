6139d3851da71e7164cd4ed505049bfb790c25f6
Anwarkhan31 <144197571+Anwarkhan31@users.noreply.github.com>
2023-09-08 11:01:21 +0300

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,19 @@
+# soal 1
+print("konversi suhu celcius ke fahrenheit")
+c = int(input("masukkan celcius "))
+konversi = (c*9/5)+32
+print("hasil dari konvesi celcius ke fahrenheit = ",konversi)
+
+#soal 2
+
+print("konversi suhu fahrenheit ke celcius")
+f = int(input("masukkan fahrenheit "))
+konversi = (f-32)*5/9
+print("hasil dari konversi fahrenheit ke celcius = ",konversi)
+
+#soal 3
+
+print("konversi suhu kelvin ke celcius")
+k = int(input("masukkan kelvin "))
+konversi = k-273,15
+print("hasil dari konversi kelvin ke celcius ",konversi)
